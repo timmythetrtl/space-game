61926bbc9a9edfb137e0dba0e0678c90b20182e1
narmstrong-dev <nickarmstrong189@gmail.com>
2022-07-17 15:18:52 -0500

    added baselimb health functionality

--- LiterallyJustSpaceStationThirteen/Assets/Player Scripts/Limb Scripts/eyeRHealth.cs
+++ LiterallyJustSpaceStationThirteen/Assets/Player Scripts/Limb Scripts/eyeRHealth.cs
@@ -1,18 +1,56 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class eyeRHealth : MonoBehaviour
 {
+    
+    [SerializeField] private float baseEyeRHealth = 100;
+    [SerializeField] private float eyeRHealthCurrent;
+
+    private string eyeRID = "EyeR";
+    private bool eyeRState = true;
+    private bool validTarget = false;
     // Start is called before the first frame update
     void Start()
-    {
-        
+    {    
+        resetHealth();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void resetHealth()
+    {
+        eyeRHealthCurrent = baseEyeRHealth;
+    }
+
+    private void OnEnable()
     {
         
+        baseHealth.partDamage += takeREyeDamage;
+        baseHealth.partName += checkValidTarget;
     }
+
+    private void onDisable()
+    {
+        baseHealth.partDamage -= takeREyeDamage;
+    }
+    // Update is called once per frame
+
+    private void checkValidTarget(string targetPart)
+    {
+        if (String.Equals(eyeRID, targetPart))
+            validTarget = true;
+    }
+
+    private void takeREyeDamage(float eyeR)
+    {
+        
+        if (validTarget == true)
+        {
+            eyeRHealthCurrent -= eyeR;
+            validTarget = false;
+        }
+            
+    }
+    
 }
